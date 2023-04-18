04a441a5f1952e67d21f5a41a8fea551d7a94fbf
Gabriel Henrique <gabriel.santos@liferay.com>
2023-04-18 17:58:43 -0300

    WIp

--- modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/background/task/OptimizeImagesStatusMessageSenderUtil.java
+++ modules/apps/adaptive-media/adaptive-media-web/src/main/java/com/liferay/adaptive/media/web/internal/background/task/OptimizeImagesStatusMessageSenderUtil.java
@@ -15,20 +15,23 @@
 package com.liferay.adaptive.media.web.internal.background.task;
 
 import com.liferay.adaptive.media.constants.AMOptimizeImagesBackgroundTaskConstants;
+import com.liferay.osgi.util.service.Snapshot;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskStatusMessageSender;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskThreadLocal;
 import com.liferay.portal.kernel.backgroundtask.constants.BackgroundTaskConstants;
 import com.liferay.portal.kernel.messaging.Message;
 
 import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Sergio González
  */
-@Component(service = {})
 public class OptimizeImagesStatusMessageSenderUtil {
+
+	public static final Snapshot<BackgroundTaskStatusMessageSender>
+		_backgroundTaskStatusMessageSenderSnapshot = new Snapshot<>(
+			OptimizeImagesStatusMessageSenderUtil.class,
+			BackgroundTaskStatusMessageSender.class);
 
 	public static void sendStatusMessage(
 		String phase, long companyId, String configurationEntryUuid) {
@@ -58,15 +61,14 @@ public class OptimizeImagesStatusMessageSenderUtil {
 		message.put(AMOptimizeImagesBackgroundTaskConstants.PHASE, phase);
 		message.put("status", BackgroundTaskConstants.STATUS_IN_PROGRESS);
 
-		_backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
+		BackgroundTaskStatusMessageSender backgroundTaskStatusMessageSender =
+			_backgroundTaskStatusMessageSenderSnapshot.get();
+
+		backgroundTaskStatusMessageSender.sendBackgroundTaskStatusMessage(
 			message);
 	}
 
 	private static OptimizeImagesStatusMessageSenderUtil
 		_optimizeImagesStatusMessageSenderUtil;
-
-	@Reference
-	private BackgroundTaskStatusMessageSender
-		_backgroundTaskStatusMessageSender;
 
 }
